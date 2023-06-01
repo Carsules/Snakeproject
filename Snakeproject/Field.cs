bd150b42c23533d9c0ac1a742caa76984c544b5c
Илья Мягкой <s2104143@edu.misis.ru>
2023-06-01 19:53:40 +0300

    Field

--- Snakeproject/Field.cs
+++ Snakeproject/Field.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Snakeproject
 {
-    class Class1
+    class Field
     {
+        byte[,] zone = new byte[25, 25];
+
+        enum stateofsquare
+        {
+            empty=0,
+            snake=1,
+            food=2,
+            edge=3
+        };
+
+        void Edgezone()
+        {
+            for(byte i = 0; i < 25; i++)
+            {
+                zone[i,0]=(byte)stateofsquare.edge;
+                zone[i, 24] = (byte)stateofsquare.edge;
+                zone[0, i] = (byte)stateofsquare.edge;
+                zone[24, i] = (byte)stateofsquare.edge;
+            }
+        }
+        void shootfood()
+        {
+            bool same = true;
+            while (same)
+            {
+                Random x = new Random();
+                Random y = new Random();
+                if (zone[x.Next(25), y.Next(25)] != (byte)stateofsquare.snake)
+                {
+                    same = false;
+                }
+            }
+
+        }
     }
 }
